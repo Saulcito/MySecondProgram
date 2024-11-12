77a9843ed0a2ae1a6e946b545b3d76142eb2cb7f
saulp <saulp@LAPTOP-ESAIP1DN>
2024-11-12 14:49:25 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySecondProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3; 
+            int b = 4;
+            double result;
+            result = ((Math.Pow(a + b, 2) - (Math.Pow(a, 2) + 2 * a * b)) / Math.Pow(b, 2));
+            Console.WriteLine("result = {0}", result);
+            Console.ReadKey();
+        }
+    }
+}
